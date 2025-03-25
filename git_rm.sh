7185b711752f4113efbb12a31578adfce02e5935
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2025-03-25 09:39:57 +0900

    Complete external gem migration and add Ruby environment files
    - Add .ruby-version (3.4.1) to specify required Ruby version
    - Add Gemfile.lock to lock dependency versions
    - Remove filter_llm_generate.rb and out_sstp.rb completely as they're now used as external gems
    - Add temporary utility scripts (will be removed in future)

---
+++ git_rm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /Users/bash/src/rowaia
+git rm lib/fluent/plugin/filter_llm_generate.rb
+git rm lib/fluent/plugin/out_sstp.rb
---
+++ tmp_delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm /Users/bash/src/rowaia/lib/fluent/plugin/filter_llm_generate.rb
+rm /Users/bash/src/rowaia/lib/fluent/plugin/out_sstp.rb
