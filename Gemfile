47daef380d47afed0f1bd49f575f8c23802827d4
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2025-03-28 21:05:19 +0900

    add

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'fluentd', "~> 1.12"
 gem 'llmalfr', github: 'bash0C7/llmalfr'
 gem 'fluent-plugin-llm-generate', github: 'bash0C7/fluent-plugin-llm-generate'
 gem 'fluent-plugin-sstp', github: 'bash0C7/fluent-plugin-sstp'
+gem 'fluent-plugin-context', path: './lib/fluent-plugin-context'
--- lib/fluent-plugin-context/lib/fluent/plugin/in_context.rb
+++ lib/fluent-plugin-context/lib/fluent/plugin/in_context.rb
@@ -1,11 +1,58 @@
-
-
-require "fluent/plugin/input"
+require 'fluent/plugin/input'
 
 module Fluent
   module Plugin
-    class ContextInput < Fluent::Plugin::Input
-      Fluent::Plugin.register_input("context", self)
+    class ContextInput < Input
+      Fluent::Plugin.register_input('context', self)
+
+      desc 'Path to the context files directory'
+      config_param :path, :string
+
+      desc 'Tag for the emitted events'
+      config_param :tag, :string, default: nil
+
+      desc 'Run interval in seconds'
+      config_param :run_interval, :time, default: 1800 # 30 minutes
+
+      def configure(conf)
+        super
+        raise Fluent::ConfigError, "path parameter is required" unless @path
+      end
+
+      def start
+        super
+        @running = true
+        @thread = Thread.new(&method(:run))
+      end
+
+      def shutdown
+        @running = false
+        @thread.join
+        super
+      end
+
+      def run
+        while @running
+          process_files
+          sleep @run_interval
+        end
+      end
+
+      def process_files
+        Dir.glob(File.join(@path, '*')).each do |file|
+          next if File.directory?(file)
+          
+          begin
+            content = File.read(file)
+            emit_tag = @tag || File.basename(file, '.*')
+            time = Fluent::Engine.now
+            record = { 'message' => content, 'file' => file }
+            router.emit(emit_tag, time, record)
+          rescue => e
+            log.error "Failed to process file: #{file}", error: e.to_s
+          end
+        end
+      end
     end
   end
 end
